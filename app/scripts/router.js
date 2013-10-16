e519af1037fe628ed5997f7b812c2753b2125f59
K4sa <vuorio.ville@gmail.com>
2013-10-16 18:03:19 +0300

    lisätty kuvaView

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,5 +1,5 @@
 /*global define*/
-define(['backbone', 'views/map-view', 'views/haaste-view'], function(Backbone, MapView, HaasteView) {
+define(['backbone', 'views/map-view', 'views/haaste-view', 'views/kuva-view'], function(Backbone, MapView, HaasteView, KuvaView) {
 
     'use strict';
 
@@ -21,6 +21,10 @@ define(['backbone', 'views/map-view', 'views/haaste-view'], function(Backbone, M
             case 'haaste':
                 this.haasteView = this.haasteView || new HaasteView();
                 $('#page').html(this.haasteView.render().el);
+                break;
+            case 'kuva':
+                this.kuvaView = this.kuvaView || new kuvaView();
+                $('#page').html(this.kuvaView.render().el);
                 break;
             default:
                 console.log('Tuntematon sivu ' + page);
