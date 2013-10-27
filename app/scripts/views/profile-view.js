fd14c236327d7652a5bc54791aa7456bb7b524dd
Laura-Maria Perkaus <laura-mariaperkaus@Laura-Maria-Perkaus-MacBook-Pro.local>
2013-10-27 22:32:35 +0200

    Profiilinäkymään lisää juttuja

--- app/scripts/views/profile-view.js
+++ app/scripts/views/profile-view.js
@@ -3,12 +3,15 @@ define([
     'backbone',
     'underscore',
     'collections/photos-collection',
-    'hbs!tmpl/profiili'
-], function(Backbone, _, PhotosCollection, Template) {
+    'hbs!tmpl/profiili',
+    'hbs!tmpl/feed-item',
+    'views/photo-view'
+], function(Backbone, _, PhotosCollection, Template, FeedTemplate, PhotoView) {
     'use strict';
 
     var ProfileView = Backbone.View.extend({
         template: Template,
+        feedItemTemplate: FeedTemplate,
 
         initialize: function() {
             this.collection.fetch();
@@ -21,10 +24,33 @@ define([
             }, this);
 
             this.el.innerHTML = this.template({ photos: userPhotos });
-            console.log(this.collection.toJSON());
+            this.renderFeed();
             return this;
+        },
+
+        renderFeed: function() {
+            var feedItems = [
+                { otsikko: 'Uusi haaste', teksti: 'Kaverisi Kamu lisäsi uuden <a href="/photos/573912288630846083_454871195">haasteen</a>'},
+                { otsikko: 'Ilmoittautuminen', teksti: 'Ilmoittauduit tapahtumaan Höntsäilypesistä Ahvenisjärven kentällä.'},
+                { otsikko: 'Item', teksti: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quas, sapiente, rerum deserunt nemo cupiditate dolorum a dignissimos commodi natus facilis quaerat magni numquam id laudantium aut ad doloribus ex?'},
+                { otsikko: 'Item', teksti: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quas, sapiente, rerum deserunt nemo cupiditate dolorum a dignissimos commodi natus facilis quaerat magni numquam id laudantium aut ad doloribus ex?'},
+                { otsikko: 'Item', teksti: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quas, sapiente, rerum deserunt nemo cupiditate dolorum a dignissimos commodi natus facilis quaerat magni numquam id laudantium aut ad doloribus ex?'},
+                { otsikko: 'Item', teksti: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, quas, sapiente, rerum deserunt nemo cupiditate dolorum a dignissimos commodi natus facilis quaerat magni numquam id laudantium aut ad doloribus ex?'},
+            ];
+
+            _.each(feedItems, this.renderFeedItem, this);
+            return this;
+        },
+
+        renderFeedItem: function(item) {
+            this.$('.newsfeed').append(this.feedItemTemplate(item));
+        },
+
+        renderPhoto: function(photo) {
+            if (photo.get('location') !== null) {
+                this.el.appendChild( new PhotoView({ model: photo }).render().el );
+            }
         }
     });
-
-    return ProfileView;
+return ProfileView;
 });
